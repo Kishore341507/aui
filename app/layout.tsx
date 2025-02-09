f25fb82cc2ea66d13f9fcac1ef2ebf899c80f43d
kishore341507 <choudhary341507@gmail.com>
2025-02-09 23:52:21 +0530

    added meta data for embeding

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "AUI",
-  description: "AUI (Among Us India) is India's most active Discord server, bringing together a vibrant and engaging gaming community. We host regular events, tournaments, and game nights, fostering a fun and competitive environment. Whether you're looking for casual or competitive gameplay, AUI is the perfect place to connect with fellow gamers and be part of an ever-growing community." ,
-  authors : [ {name : "Team AUI" , url : "https://amongusindia.com/aboutus"} ],
-  keywords: ["amongusindia" , 'AUI' , 'AUI discord' , 'among us india' , 'among us indians' , 'among us india discord' , 'among us india server' , 'among us india discord server' , 'among us india discord community' , 'among us india discord community server' , 'among us india discord community server invite' , 'among us india discord community server link' , 'among us india discord community server link invite' , 'among us india discord community server invite link'],
-  creator : "Kishore",
+  description: "AUI (Among Us India) is India's most active Discord server, bringing together a vibrant and engaging gaming community. We host regular events, tournaments, and game nights, fostering a fun and competitive environment. Whether you're looking for casual or competitive gameplay, AUI is the perfect place to connect with fellow gamers and be part of an ever-growing community.",
+  authors: [{ name: "Team AUI", url: "https://amongusindia.com/aboutus" }],
+  keywords: ["amongusindia", 'AUI', 'AUI discord', 'among us india', 'among us indians', 'among us india discord', 'among us india server', 'among us india discord server', 'among us india discord community', 'among us india discord community server', 'among us india discord community server invite', 'among us india discord community server link', 'among us india discord community server link invite', 'among us india discord community server invite link'],
+  creator: "Kishore",
+  openGraph: {
+    title: "AUI",
+    description: "AUI (Among Us India) is India's most active Discord server, bringing together a vibrant and engaging gaming community. We host regular events, tournaments, and game nights, fostering a fun and competitive environment. Whether you're looking for casual or competitive gameplay, AUI is the perfect place to connect with fellow gamers and be part of an ever-growing community.",
+    url: "https://amongusindia.com",
+    images: [
+      {
+        url: "/aui.jpg",
+        width: 256,
+        height: 256,
+        alt: "AUI Logo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -25,13 +38,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              disableTransitionOnChange
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
         >
-            <NavBar />
-            {children}
-            <Analytics />
+          <NavBar />
+          {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
