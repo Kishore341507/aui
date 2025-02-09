127980df605fc3400b397d3acd272c017fe5af96
kishore341507 <choudhary341507@gmail.com>
2025-02-09 14:54:23 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import { ThemeProvider } from "@/components/theme-provider"
 import NavBar from "./components/nav-bar";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         >
             <NavBar />
             {children}
+            <Analytics />
         </ThemeProvider>
       </body>
     </html>
