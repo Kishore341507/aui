c84773e1394f5ef0ce61cb75454ec1b460f9f88c
Kamal Kishore <choudhary341507@gmail.com>
2026-02-14 18:47:18 +0530

    Revert "Formv3.0"

--- app/forms/event-team/page.tsx
+++
@@ -1,9 +0,0 @@
-import EventTeamForm from "./event-team-form";
-
-export const metadata = {
-  title: "Event Team Application - AUI Forms",
-};
-
-export default function Page() {
-  return <EventTeamForm />;
-}
