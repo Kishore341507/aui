013711436cd24af2014bc70f05621234dd8fd906
Aman Maurya <amanmaurya917@gmail.com>
2026-03-15 10:51:53 +0000

    herosection image

--- app/membership/page.tsx
+++ app/membership/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import MembershipCards from "./membership-cards"
 import { TawkChat } from "@/components/tawk-chat"
 import prisma from "@/prisma/db"
+import Image from "next/image"
 
 export const metadata: Metadata = {
   title: "Membership Tiers - AUI | India's Most Active Discord Server",
@@ -31,6 +32,28 @@ export default async function Page() {
 
   return (
     <>
+      <section
+          className="relative min-h-[10vh] flex items-center justify-center overflow-hidden"
+        >
+    
+    
+          {/* Background */}
+          <Image
+            src="/heroback.avif"
+            alt="Hero background"
+            fill
+            priority
+            className="object-cover"
+          />
+            {/* content */}
+            <div className="relative z-10 max-w-4xl h-[20vh] m-auto px-4 text-center ">
+                <h1 className="text-4xl md:text-5xl font-bold text-white">Membership Tiers</h1>
+                <p className="mt-4 text-lg md:text-xl text-white/90">
+                    Choose the tier that&apos;s right for you.
+                </p>
+            </div>
+    
+        </section>
       <MembershipCards diamondSoldCount={diamondSoldCount} />
       <TawkChat />
     </>
